c5b837c12b1a8bdcafff00671764b5816bae3fb5
Ondrej Vana <ondrej.vana@keboola.com>
2015-09-21 18:42:16 -0700

    feat: API update to 2.4

--- FacebookAdsExtractor.php
+++ FacebookAdsExtractor.php
@@ -32,7 +32,7 @@ class FacebookAdsExtractor extends JsonExtractor
 	public function run(Config $config) {
 		$client = new Client(
 			[
-				'base_url' => 'https://graph.facebook.com/v2.3/',
+				'base_url' => 'https://graph.facebook.com/v2.4/',
 				'defaults' => [
 					'query' => ['access_token' => $config->getAttributes()['access_token']]
 				]
